ac714f49d172d6d74ac386e6a52a29a15e749326
nachodf99 <45067132+nachodf99@users.noreply.github.com>
2026-02-10 01:02:55 +0100

    Add hero diagnostic form

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/diagnostico", (req, res) => {
+  res.redirect("/#contacto");
 });
 
 app.listen(PORT, () => {
